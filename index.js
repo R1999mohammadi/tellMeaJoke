315120eab77c665a2ad17d07be0faf83579f0c73
Rafi <rafi1999mohammadi@gmail.com>
2023-06-13 00:46:01 -0400

    index.html task completed successfully

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const jokes = [
+    'Why did the melon jump into the lake? It wanted to be a water-melon',
+    'Grandma: Back in our days, you could buy bread, milk, soaps, spices, eggs, meat, all for a dollarLittle Kid: You can’t do that now. They have CCTVs everywhere',
+    'You can\’t trust atoms. They make up everything!',
+    ' What do you call an elephant that doesn\’t matter? An irrelephant.',
+    'Why do you smear peanut butter on the road? To go with the traffic jam.',
+    'Where did Napoleon keep his armies? In his sleevies',
+    
+
+]
+
+let joke = document.querySelector('.joke')
